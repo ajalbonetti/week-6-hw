051bd538e9905f2c96796248df812ae21481e760
Aaron A <albon101@Aarons-MacBook-Pro.local>
2016-08-10 17:58:06 -0700

    fixed submit button

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -18,12 +18,57 @@ $('#addMovie').on('click', function(){
 	var movie = $('#movieInput').val().trim();
 	topics.push(movie);
 	createButtons();
+	$('.topic').on('click', function() {
+
+	$('#movies').empty();
+	var movie = $(this).attr('data-name');
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=dc6zaTOxFJmzC&limit=10&rating=pg-13";
+
+	$.ajax({
+		url: queryURL,
+		method: 'GET'
+	})
+	.done(function(response) {
+
+		var results = response.data;
+
+		for (var i = 0; i < results.length; i++) {
+			var $movieDiv = $('<div>')
+			var $text = $('<h2>').text("Rating: " + results[i].rating)
+			var $movieImage = $('<img>')
+			$movieImage.attr('src', results[i].images.fixed_height.url)
+			$movieImage.attr('data-still', results[i].images.fixed_height_still.url)
+			$movieImage.attr('data-animate', results[i].images.fixed_height.url)
+			$movieImage.addClass('movieImage')
+
+			$movieDiv.append($text)
+			$movieDiv.append($movieImage)
+
+			$('#movies').prepend($movieDiv)
+		}
+
+		$('.movieImage').on('click', function() {
+			var $still = $(this).attr('data-still');
+			var $animate = $(this).attr('data-animate');
+			var $src = $(this).attr('src');
+
+			if ($src === $animate) {
+				$(this).attr('src', $still) 
+			} else {
+				$(this).attr('src', $animate)
+			}
+
+		});
+
+	});
+
+	});
 	return false;
 })
 
 createButtons();
 
-$('button').on('click', function() {
+$('.topic').on('click', function() {
 
 	$('#movies').empty();
 	var movie = $(this).attr('data-name');
